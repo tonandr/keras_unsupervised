4b909b9fe09abeb5e30f9c79524068752d31686a
gutom <gutomitai@gmail.com>
2020-10-13 02:07:57 +0900

    -transformer.py are modified.

--- ku/composite_layer/transformer.py
+++ ku/composite_layer/transformer.py
@@ -21,7 +21,7 @@ class Transformer(Layer):
                  , d_output
                  , dropout_rate
                  , similarity_type=SIMILARITY_TYPE_SCALED
-                 , layer_normal_f=True
+                 , layer_norm_f=True
                  , **kwargs):
         super(Transformer, self).__init__(**kwargs)
 
@@ -41,26 +41,26 @@ class Transformer(Layer):
         self.d_output = d_output
         self.dropout_rate = dropout_rate
         self.similarity_type = similarity_type
-        self.layer_norm_f = layer_normal_f
+        self.layer_norm_f = layer_norm_f
 
         # Design layers.
         self.mh_attention_1 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.dropout_1 = Dropout(self.dropout_rate)
         self.add_1 = Add()
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             self.layer_norm_1 = LayerNormalization(epsilon=1e-6)
 
         self.mh_attention_2 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.dropout_2 = Dropout(self.dropout_rate)
         self.add_2 = Add()
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             self.layer_norm_2 = LayerNormalization(epsilon=1e-6)
 
         self.dense_3_1 = Dense(int(self.d_output * 4), activation='swish')
         self.dense_3_2 = Dense(self.d_output, activation='linear')
         self.dropout_3 = Dropout(self.dropout_rate)
         self.add_3 = Add()
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             self.layer_norm_3 = LayerNormalization(epsilon=1e-6)
 
     def call(self, inputs, training=None):
@@ -70,20 +70,20 @@ class Transformer(Layer):
         x2 = self.mh_attention_1([x, x, x, m])
         x2 = self.dropout_1(x2, training=training)
         x2 = self.add_1([x, x2])
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             x2 = self.layer_norm_1(x2)
 
         x3 = self.mh_attention_2([x2, x2, x2, m])
         x3 = self.dropout_1(x3, training=training)
         x3 = self.add_2([x2, x3])
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             x3 = self.layer_norm_2(x3)
 
         x4 = self.dense_3_1(x3)
         x4 = self.dense_3_2(x4)
         x4 = self.dropout_3(x4, training=training)
         x4 = self.add_3([x3, x4])
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             x4 = self.layer_norm_3(x4)
         outputs = x4
 
@@ -108,7 +108,7 @@ class InterferedTransformer(Layer):
                  , d_output
                  , dropout_rate
                  , similarity_type=SIMILARITY_TYPE_SCALED
-                 , layer_normal_f=True
+                 , layer_norm_f=True
                  , **kwargs):
         super(InterferedTransformer, self).__init__(**kwargs)
 
@@ -127,27 +127,27 @@ class InterferedTransformer(Layer):
         self.num_head = num_head
         self.d_output = d_output
         self.similarity_type = similarity_type
-        self.layer_norm_f = layer_normal_f
+        self.layer_norm_f = layer_norm_f
 
         # Design layers.
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             self.layer_norm_embedded = LayerNormalization()
 
         self.mh_attention_1 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.add_1 = Add()
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             self.layer_norm_1 = LayerNormalization()
 
         self.mh_attention_2 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.add_2 = Add()
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             self.layer_norm_2 = LayerNormalization()
         self.concat_2 = Concatenate(axis=-1)
 
         self.dense_3_1 = Dense(self.d_output, activation='relu')
         self.dense_3_2 = Dense(self.d_output, activation='linear')
         self.add_3 = Add()
-        if layer_normal_f:
+        if layer_norm_f:
             self.layer_norm_3 = LayerNormalization()
         self.dropout_3 = Dropout(self.dropout_rate)
 
@@ -157,24 +157,24 @@ class InterferedTransformer(Layer):
 
         num_seq = backend.int_shape(x)[1]
         embedded = tf.tile(tf.expand_dims(inputs[0], axis=1), (1, num_seq, 1))
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             embedded = self.layer_norm_embedded(embedded)
 
         x2 = self.mh_attention_1([x, x, x, m])
         x2 = self.add_1([x, x2])
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             x2 = self.layer_norm_1(x2)
 
         x3 = self.mh_attention_2([x2, x2, x2, m])
         x3 = self.add_2([x2, x3])
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             x3 = self.layer_norm_2(x3)
         x3 = self.concat_2([x3, embedded])
 
         x4 = self.dense_3_1(x3)
         x4 = self.dense_3_2(x4)
         x4 = self.add_3([x3, x4])
-        if self.layer_normal_f:
+        if self.layer_norm_f:
             x4 = self.layer_norm_3(x4)
         x4 = self.dropout_3(x4, training=training)
         outputs = x4
