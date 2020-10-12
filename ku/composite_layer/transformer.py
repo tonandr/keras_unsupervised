1c0680f18f824a67eaaec17c865f4ad1d2abe932
gutom <gutomitai@gmail.com>
2020-10-13 01:49:15 +0900

    -transformer.py are modified.

--- ku/composite_layer/transformer.py
+++ ku/composite_layer/transformer.py
@@ -16,7 +16,13 @@ from ..layer_ext import MultiHeadAttention\
 class Transformer(Layer):
     """Transformer."""
 
-    def __init__(self, num_head, d_output, dropout_rate, similarity_type=SIMILARITY_TYPE_SCALED, **kwargs):
+    def __init__(self
+                 , num_head
+                 , d_output
+                 , dropout_rate
+                 , similarity_type=SIMILARITY_TYPE_SCALED
+                 , layer_normal_f=True
+                 , **kwargs):
         super(Transformer, self).__init__(**kwargs)
 
         # Check exception.
@@ -35,21 +41,27 @@ class Transformer(Layer):
         self.d_output = d_output
         self.dropout_rate = dropout_rate
         self.similarity_type = similarity_type
+        self.layer_norm_f = layer_normal_f
 
         # Design layers.
         self.mh_attention_1 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.dropout_1 = Dropout(self.dropout_rate)
         self.add_1 = Add()
-        self.layer_norm_1 = LayerNormalization(epsilon=1e-6)
+        if self.layer_normal_f:
+            self.layer_norm_1 = LayerNormalization(epsilon=1e-6)
 
         self.mh_attention_2 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.dropout_2 = Dropout(self.dropout_rate)
         self.add_2 = Add()
+        if self.layer_normal_f:
+            self.layer_norm_2 = LayerNormalization(epsilon=1e-6)
 
         self.dense_3_1 = Dense(int(self.d_output * 4), activation='swish')
         self.dense_3_2 = Dense(self.d_output, activation='linear')
         self.dropout_3 = Dropout(self.dropout_rate)
         self.add_3 = Add()
+        if self.layer_normal_f:
+            self.layer_norm_3 = LayerNormalization(epsilon=1e-6)
 
     def call(self, inputs, training=None):
         x = inputs[0]
@@ -58,18 +70,21 @@ class Transformer(Layer):
         x2 = self.mh_attention_1([x, x, x, m])
         x2 = self.dropout_1(x2, training=training)
         x2 = self.add_1([x, x2])
-        x2 = self.layer_norm_1(x2)
+        if self.layer_normal_f:
+            x2 = self.layer_norm_1(x2)
 
         x3 = self.mh_attention_2([x2, x2, x2, m])
         x3 = self.dropout_1(x3, training=training)
         x3 = self.add_2([x2, x3])
-        x3 = self.layer_norm_2(x3)
+        if self.layer_normal_f:
+            x3 = self.layer_norm_2(x3)
 
         x4 = self.dense_3_1(x3)
         x4 = self.dense_3_2(x4)
         x4 = self.dropout_3(x4, training=training)
         x4 = self.add_3([x3, x4])
-        x4 = self.layer_norm_3(x4)
+        if self.layer_normal_f:
+            x4 = self.layer_norm_3(x4)
         outputs = x4
 
         return outputs
@@ -79,7 +94,8 @@ class Transformer(Layer):
         config = {'num_head': self.num_head
             , 'd_output': self.d_output
             , 'dropout_rate': self.dropout_rate
-            , 'similarity_type': self.similarity_type}
+            , 'similarity_type': self.similarity_type
+            , 'layer_norm_f': self.layer_norm_f}
         base_config = super(Transformer, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
@@ -114,17 +130,17 @@ class InterferedTransformer(Layer):
         self.layer_norm_f = layer_normal_f
 
         # Design layers.
-        if layer_normal_f:
+        if self.layer_normal_f:
             self.layer_norm_embedded = LayerNormalization()
 
         self.mh_attention_1 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.add_1 = Add()
-        if layer_normal_f:
+        if self.layer_normal_f:
             self.layer_norm_1 = LayerNormalization()
 
         self.mh_attention_2 = MultiHeadAttention(num_head, d_output, dropout_rate, similarity_type=similarity_type)
         self.add_2 = Add()
-        if layer_normal_f:
+        if self.layer_normal_f:
             self.layer_norm_2 = LayerNormalization()
         self.concat_2 = Concatenate(axis=-1)
 
