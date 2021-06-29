4a8659a81f32a333007619e0f925e572165c8180
gutom <gutomitai@gmail.com>
2021-06-29 18:01:05 +0900

    -setup.py is modified including python 3.7.

--- setup.py
+++ setup.py
@@ -40,7 +40,9 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         # These classifiers are *not* checked by 'pip install'. See instead
         # 'python_requires' below.
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ],
 
     # This field adds keywords for your project which will appear on the
@@ -78,7 +80,7 @@ setup(
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='==3.6',
+    python_requires='>=3.6, <3.8',
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
