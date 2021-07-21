fd2a2494bca2eb745027178e220b42b5e5882f94
gutom <gutomitai@gmail.com>
2021-07-21 17:18:51 +0900

    -setup.py is modified.

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='=3.8',
+    python_requires='==3.8',
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
